765720a1f830a02dc0987fccf42a5439fafe9849
Derek Marsh <dmarsh@southernco.com>
2017-06-03 16:04:20 -0400

    remove manual steps from deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,3 @@
-mkdir ~/dev
-cd ~/dev
-git clone https://github.com/dmarsh19/infrastructure.git infrastructure_project
-sudo ./infrastructure_project/1install_base.sh
-./infrastructure_project/2setup_base.sh
-source ~/.bashrc
-sudo ./infrastructure_project/12install_nginx.sh
-cd /var/www
-# will install as root, needs permissions set for a web user (www-data?)
-# rename local dir in /var/www from git clone to the url? (i.e. twiml.admrsh.com)
-# the last param in git clone command is the name of the directory to be created
-# this is a barebones web app. No virtualenv.
-# and yes, maybe the entire project shouldn't be in /var/www. chmod your files right and you'll be fine.
-sudo git clone https://github.com/dmarsh19/flask_uwsgi.git
 sudo touch flask_uwsgi/settings.py
 sudo chown -R www-data:www-data /var/www/flask_uwsgi
 # sticky bit to keep new files owned by www-data; 755
